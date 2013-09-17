073e785f17f3c658a4e0f534dd156263d95a763d
lengxi <xleng@princeton.edu>
2013-09-16 20:26:01 -0400

    refactor jquery into local copy

--- user.js
+++ user.js
@@ -1,13 +1,4 @@
 (function () {
-    /* inject jquery */
-    function injectJquery() {
-        var jQueryVersion = "2.0.2";
-        var a = document.createElement("script");
-        a.src = "//ajax.googleapis.com/ajax/libs/jquery/" + jQueryVersion + "/jquery.js";
-        a.type = "text/javascript";
-        a.setAttribute("id", "jquery");
-        document.getElementsByTagName("head")[0].appendChild(a);
-    }
 
     /* run execute when condition evaluates true */
     function delayedExecute(condition, execute) {
@@ -19,7 +10,7 @@
 
     /* returns true if jquery is loaded */
     var checkJquery = function() {
-        if (window.jQuery && window.$)
+        if (window.jQuery)
             return true;
         else
             return false;
@@ -27,15 +18,15 @@
 
     /* body of application */
     var application = function() {
-        alert("hi");
+        $(document).on("click", ".ad3", function() {
+            alert("hi");
+        });
     }
 
-    injectJquery();
-
-    var a = document.createElement("script");
+    /*var a = document.createElement("script");
     a.type = "text/javascript";
     a.text = "while(true) {if(window.jQuery) alert(window.jQuery); break; }"
-    document.getElementsByTagName("head")[0].insertBefore(a, document.getElementById("jquery").nextSibling);
+    document.getElementsByTagName("head")[0].insertBefore(a, document.getElementById("jquery").nextSibling);*/
 
-    //delayedExecute(checkJquery, application);
+    delayedExecute(checkJquery, application);
 })();
